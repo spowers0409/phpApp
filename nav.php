3bac661fe2395c62e3db018d502e1107b572d383
spowers0409 <spowers0409@gmail.com>
2024-06-15 09:18:18 -0500

    added guthub button

--- nav.php
+++ nav.php
@@ -3,18 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>The PHP Application</title>
+    <title>My Web Application</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-<nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="contact.php">Contact Us</a></li>
-        <li><a href="about.php">About PHP</a></li>
-    </ul>
-</nav>
-    
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+            <li><a href="about.php">About Us</a></li>
+            <li class="github">
+                <a href="https://github.com" target="_blank">
+                    <img src="github-mark.png" alt="GitHub" class="github-logo">
+                </a>
+            </li>
+        </ul>
+    </nav>
 </body>
 </html>
